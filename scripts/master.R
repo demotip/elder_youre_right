8f2820defaa56e126cdece6dbbf8730766bd5295
LawPlastina <lawrence.plastina@mail.mcgill.ca>
2024-07-02 11:36:16 -0400

    Creating master.R which runs all scripts for this project at once, in order

---
+++ scripts/master.R
@@ -0,0 +1,16 @@
+# Running everything all at once
+
+source("scripts/clean_and_combine_data.R")
+source("scripts/append_age_differences.R")
+source("scripts/variable_labels.R")
+source("scripts/balance_tables.R")
+source("scripts/descriptive_statistics.R")
+source("scripts/contrasts.R")
+source("scripts/plots.R")
+source("scripts/comparison_plots.R")
+source("scripts/contrasts_logistic.R")
+source("scripts/cross_validation.R")
+source("scripts/presentable_table.R")
+source("scripts/regression_table.R")
+source("scripts/results_tables.R")
+
